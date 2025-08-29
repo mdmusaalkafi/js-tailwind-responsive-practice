4d387f432d5144e0c63dd3eb15a3830ca84d9201
mdmusaalkafi <97037598+mdmusaalkafi@users.noreply.github.com>
2025-08-29 19:45:04 +0600

    counter initialize

---
+++ script.js
@@ -0,0 +1,8 @@
+let heartCount = 0;
+  let coinCount = 100;
+  let copyCount = 2;
+
+  let heartCountEl = document.getElementById("heart-count");
+  let coinCountEl = document.querySelector("section .flex.gap-2 div:nth-child(2) h1");
+  let copyCountEl = document.querySelector("section .flex.gap-2 div:nth-child(3) h1");
+  
