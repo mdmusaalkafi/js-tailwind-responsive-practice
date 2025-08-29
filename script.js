f7e91586085e3f8c18c472f42473143ca2e6b0ad
mdmusaalkafi <97037598+mdmusaalkafi@users.noreply.github.com>
2025-08-29 19:46:25 +0600

    call history column

--- script.js
+++ script.js
@@ -5,4 +5,10 @@ let heartCount = 0;
   let heartCountEl = document.getElementById("heart-count");
   let coinCountEl = document.querySelector("section .flex.gap-2 div:nth-child(2) h1");
   let copyCountEl = document.querySelector("section .flex.gap-2 div:nth-child(3) h1");
+
+  let historyCol = document.querySelector(".row-span-3");
+  let clearBtn = historyCol.querySelector("button");
+  let historyList = document.createElement("ul");
+  historyCol.appendChild(historyList);
+
   
